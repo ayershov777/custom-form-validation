01c294317d25d0020bcdc1cb5f75196a20c90578
Anton Yershov <anthony.yershov@gmail.com>
2019-09-18 17:52:32 -0400

    cleaned everything up, ready to deploy

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ require('./config/database');
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/api/users', require('./routes/api/users'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
