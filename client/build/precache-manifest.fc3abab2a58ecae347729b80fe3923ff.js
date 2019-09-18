22802534cd86ad8fabecc78c90f07b22ec63ea7a
Anton Yershov <anthony.yershov@gmail.com>
2019-09-18 18:00:42 -0400

    removed build from .gitignore

---
+++ client/build/precache-manifest.fc3abab2a58ecae347729b80fe3923ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4ac0a4c5ba3e8898f86b0dea20188f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae7c5ea93cee6ac7d859",
+    "url": "/static/css/main.feb997ab.chunk.css"
+  },
+  {
+    "revision": "1ebe65929cc07cd469be",
+    "url": "/static/js/2.b1997445.chunk.js"
+  },
+  {
+    "revision": "ae7c5ea93cee6ac7d859",
+    "url": "/static/js/main.4d0e1b23.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
